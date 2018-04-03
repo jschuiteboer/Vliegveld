abe441d0a9f8534d08f2695418fb94636b095d1d
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-04-03 09:09:36 +0200

    initial commit

---
+++ src/main/java/com/yacht/vliegveld/VliegveldApplication.java
@@ -0,0 +1,12 @@
+package com.yacht.vliegveld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VliegveldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VliegveldApplication.class, args);
+	}
+}
