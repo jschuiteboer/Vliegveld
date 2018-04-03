351f74c9fc16408b5057e24eedd39624be2fb85c
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-04-03 10:29:52 +0200

    Added modal form + loading and clearing form data

--- src/main/resources/static/js/airplanes.js
+++ src/main/resources/static/js/airplanes.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
     const restEndpoint = '/api/airplanes/';
 
     var _tableElement = $('#airplanesTable');
+    var _addButton    = $('#addButton');
+    var _modalElement = $('#airplaneModal');
+    var _inputId      = _modalElement.find('#id');
+    var _inputFuel    = _modalElement.find('#fuel');
 
     var _dataTable = _tableElement.DataTable({
         ajax: {
@@ -20,4 +24,30 @@ $(document).ready(function() {
             },
         ]
     });
+
+    _tableElement.find('tbody').on('click', 'tr', function() {
+        var data = _dataTable.row(this).data();
+
+        openModal(data);
+    });
+
+    _addButton.click(function() {
+        openModal(null);
+    });
+
+    /**
+     * Opens the modal and sets the form values.
+     * @param airplane - the airplane object, null for an empty form
+     */
+    function openModal(airplane) {
+        if(airplane) {
+            _inputId.val(airplane.id);
+            _inputFuel.val(airplane.fuel);
+        } else {
+            _inputId.val("");
+            _inputFuel.val("");
+        }
+
+        _modalElement.modal('show');
+    }
 });
