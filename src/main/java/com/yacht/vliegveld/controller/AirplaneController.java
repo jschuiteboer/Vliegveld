9fca34fe61291de123a8a2c59992d2c63066224e
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-04-03 09:54:11 +0200

    added getAll endpoint

--- src/main/java/com/yacht/vliegveld/controller/AirplaneController.java
+++ src/main/java/com/yacht/vliegveld/controller/AirplaneController.java
@@ -1,12 +1,21 @@
 package com.yacht.vliegveld.controller;
 
+import com.yacht.vliegveld.model.Airplane;
 import com.yacht.vliegveld.repository.AirplaneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/airplanes")
 public class AirplaneController {
 
     @Autowired
     AirplaneRepository airplaneRepository;
+
+    @GetMapping
+    public Iterable<Airplane> getAll() {
+        return this.airplaneRepository.findAll();
+    }
 }
--- src/main/resources/static/js/airplanes.js
+++ src/main/resources/static/js/airplanes.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
+    const restEndpoint = '/api/airplanes/';
+
     var _tableElement = $('#airplanesTable');
 
-    _tableElement.DataTable({
+    var _dataTable = _tableElement.DataTable({
+        ajax: {
+            url: restEndpoint,
+            dataSrc: "",
+            type: "GET",
+        },
         columns: [
             {
                 title: "ID",
                 data: "id",
             },
             {
-                title: "Feul Level",
+                title: "Fuel Level",
                 data: "fuel",
             },
         ]
