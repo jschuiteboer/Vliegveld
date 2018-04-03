a45f2aade4a61cfb92d60d10200e317ad35f9f80
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-04-03 10:51:15 +0200

    added rest endpoints for locations

--- src/main/java/com/yacht/vliegveld/controller/LocationController.java
+++ src/main/java/com/yacht/vliegveld/controller/LocationController.java
@@ -1,12 +1,24 @@
 package com.yacht.vliegveld.controller;
 
+import com.yacht.vliegveld.model.Location;
 import com.yacht.vliegveld.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/locations")
 public class LocationController {
 
     @Autowired
     LocationRepository locationRepository;
+
+    @GetMapping
+    public Iterable<Location> getAll() {
+        return this.locationRepository.findAll();
+    }
+
+    @PutMapping
+    public Location save(@RequestBody Location airplane) {
+        return this.locationRepository.save(airplane);
+    }
 }
