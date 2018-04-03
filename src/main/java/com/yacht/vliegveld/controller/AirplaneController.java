5e24ab0ad36876c9865727a81eaf3d58a4c15671
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-04-03 09:57:18 +0200

    added save endpoint

--- src/main/java/com/yacht/vliegveld/controller/AirplaneController.java
+++ src/main/java/com/yacht/vliegveld/controller/AirplaneController.java
@@ -3,9 +3,7 @@ package com.yacht.vliegveld.controller;
 import com.yacht.vliegveld.model.Airplane;
 import com.yacht.vliegveld.repository.AirplaneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/airplanes")
@@ -17,5 +15,10 @@ public class AirplaneController {
     @GetMapping
     public Iterable<Airplane> getAll() {
         return this.airplaneRepository.findAll();
+    }
+
+    @PutMapping
+    public Airplane save(@RequestBody Airplane airplane) {
+        return this.airplaneRepository.save(airplane);
     }
 }
